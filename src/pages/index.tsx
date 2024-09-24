e750e0fb964f428ef549b9340761c7dfb61f079f
phamdinhthanhcntt6 <90460582+phamdinhthanhcntt6@users.noreply.github.com>
2024-09-24 14:12:20 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import React, { Suspense } from "react";
+import { List, Page, Icon, useNavigate } from "zmp-ui";
+import UserCard from "components/user-card";
+import { getUserID } from "zmp-sdk/apis";
+
+getUserID({
+  success: (data) => {
+    // xử lý khi gọi api thành công
+    const userID = data;
+    console.log(userID);
+  },
+  fail: (error) => {
+    // xử lý khi gọi api thất bại
+    console.log(error);
+  },
+});
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  return (
+    <Page className="page">
+      <Suspense>
+        <div className="section-container">
+          <UserCard />
+        </div>
+      </Suspense>
+      <div className="section-container">
+        <List>
+          <List.Item
+            onClick={() => navigate("/about")}
+            suffix={<Icon icon="zi-arrow-right" />}
+          >
+            <div>About</div>
+          </List.Item>
+          <List.Item
+            onClick={() => navigate("/user")}
+            suffix={<Icon icon="zi-arrow-right" />}
+          >
+            <div>User</div>
+          </List.Item>
+        </List>
+      </div>
+    </Page>
+  );
+};
+
+export default HomePage;
